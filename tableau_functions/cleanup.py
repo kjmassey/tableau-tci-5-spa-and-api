aae57c9595a0208c51c8a907c37930c7ef7c85d1
Kyle Massey <kyle.massey@gmail.com>
2023-02-15 23:31:27 -0500

    Add req_options example

--- tableau_functions/cleanup.py
+++ tableau_functions/cleanup.py
@@ -16,6 +16,13 @@ def filter_items_on_tableau_server(req_data):
         all_wbs = server.workbooks.all()
         
         # Look up TSC.RequestOptions! -- it works for text/string filters, but struggles with dates that have been serialized/decoded multiple times
+        # EX
+        # req_options = TSC.RequestOptions()
+        # req_options.filter.add(TSC.Filter(TSC.RequestOptions.Field.UpdatedAt,TSC.RequestOptions.Operator.LessThan,datetime.strptime('2022-12-29T00:00:00','%Y-%m-%dT%H:%M:%S')))
+
+        # some_wbs, p = server.workbooks.get(req_options=req_options)
+        # print([wb.__dict__ for wb in some_wbs])
+
         filtered_wbs = list(filter(lambda x: x.updated_at.timestamp() < datetime.strptime(req_data['selectedDate'].split('T')[0],'%Y-%m-%d').timestamp(),all_wbs))
 
         # Only send the text-ified details of each workbook
